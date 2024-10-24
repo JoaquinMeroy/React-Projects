767e6cce7e921b8afd78656010ee42c070ac7856
Joaquin Meroy <joaquinameroy@gmail.com>
2024-10-24 20:37:55 +0800

    Added Header
    Added new header and added the design to it

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+  return(
+    <header>
+      <div>
+        <img src="" alt="logo" />
+      </div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <button type="submit">Contact</button>
+    </header>
+  );
+}
+export default Header
